ee181453b82c0313e51eb8f387dda31500bba7eb
xdu <du.xiaoming86@gmail.com>
2024-07-24 18:09:42 +0200

    - display all the entries - delete an entry

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
 import json
 import random
@@ -28,6 +28,10 @@ def index():
     """
     examples = Example.query.order_by(func.random()).limit(5).all()
     return render_template('index.html', examples=examples)
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 
 @app.route('/add_example', methods=['POST'])
@@ -68,15 +72,51 @@ def display_examples():
             loaded_from_db = False
 
             # Save the entry and examples to the database
-            entry = Entry(entry_id=entry_id)
-            db.session.add(entry)
-            for example_obj in example_objects:
-                example_obj.entry = entry
-                db.session.add(example_obj)
-            db.session.commit()
+            if example_objects:
+                entry = Entry(entry_id=entry_id)
+                db.session.add(entry)
+                for example_obj in example_objects:
+                    example_obj.entry = entry
+                    db.session.add(example_obj)
+                db.session.commit()
 
-        return render_template('display_examples.html', examples=example_objects, loaded_from_db=loaded_from_db)
+        return render_template('display_examples.html', entry_id=entry_id, 
+                               examples=example_objects, loaded_from_db=loaded_from_db)
     return jsonify({'message': 'ID parameter is required'}), 400
+
+@app.route('/entries')
+def entries():
+    """
+    Renders the entries.html template with all the entries saved in the database.
+
+    Returns:
+        flask.Response: The rendered HTML template.
+    """
+    all_entries = Entry.query.all()
+    return render_template('entries.html', entries=all_entries)
+
+@app.route('/delete_entry/<string:entry_id>', methods=['DELETE'])
+def delete_entry(entry_id):
+    """
+    Deletes an entry with the given entry_id and all linked examples.
+
+    Args:
+        entry_id (int): The ID of the entry to delete.
+
+    Returns:
+        flask.Response: The JSON response indicating the success or failure of the deletion.
+    """
+    entry = Entry.query.filter_by(entry_id=entry_id).first()
+    if entry:
+        # Delete all associated examples
+        Example.query.filter_by(entry_id=entry.id).delete()
+
+        # Delete the entry
+        db.session.delete(entry)
+        db.session.commit()
+        return jsonify({'message': 'Entry deleted successfully'}), 200
+    else:
+        return jsonify({'message': 'Entry not found'}), 404
 
 if __name__ == '__main__':
     with app.app_context():
